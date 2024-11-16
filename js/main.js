af9c16cf01873e55c3c6ab20d1bd6374d7ad2965
YomnaSalama <yomnasalama2000@gmail.com>
2024-11-16 14:39:53 +0200

    Add the project

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var quotes = ["You miss 100% of the shots you don't take.", "It's not what happens to you, but how you react to it that matters.", "The best revenge is massive success.",
+    "Resentment is like drinking poison and waiting for your enemies to die.", "Do not take life too seriously. You will not get out alive."
+]
+var teller = ["--Wayne Gretzy", "--Epictetus", "--Frank Sinatra", "--Nelson Mandela", "--Elbert Hubbard"]
+
+
+function generateRandomNumber() {
+    var newNumber;
+    var lastNumber;
+    do {
+        newNumber = Math.floor(Math.random() * 5);
+    } while (newNumber === lastNumber);
+
+    lastNumber = newNumber;
+    return newNumber;
+}
+
+
+function generateNewQuote() {
+    var randomNumber = generateRandomNumber();
+    document.getElementById("quote").innerHTML = '"'+quotes[randomNumber]+'"';
+    document.getElementById("author").innerHTML = '"'+teller[randomNumber]+'"';
+}
+
